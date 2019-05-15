7b46223ab6723957e3affe0a134f906ce7d96f81
Alexander Shutalyov <alexander.sh0810@gmail.com>
2019-05-16 00:25:08 +0300

    slice 2x

--- sliceTest.go
+++ sliceTest.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"golang.org/x/tour/pic"
+)
+
+var dx = 5
+var dy = 5
+
+func Pic(dx, dy int) [][]uint8 {
+
+	inte := make([]uint8, dx)
+	exte := make([][]uint8, dy)
+
+	for j,_ := range exte {
+		for i,_ := range inte {
+			inte[i] = uint8(i*j)
+		}
+		exte[j] = inte
+	}
+
+	return exte
+
+}
+
 func main() {
-	
+	pic.Show(Pic)
 }
