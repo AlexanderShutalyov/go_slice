8b1bec388f890a43f1a2efedaf85c7c45bfe24d4
Alexander Shutalyov <alexander.sh0810@gmail.com>
2019-05-23 17:23:36 +0300

    task 1,2 added

--- mutation.go
+++ mutation.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
+var dnkFirst = "GAGCCTACTAACGGGAT"
+var dnkSecond = "CATCGTAATGACGGCCT"
+
+func checkstr() bool{
+	if len(dnkFirst) != len (dnkSecond) {
+		return false
+	}
+	return true
+}
+
 func main() {
-	
+
+	count:=0
+
+	if checkstr() {
+		for i, v := range dnkFirst {
+			if strings.EqualFold(string(v), string(dnkSecond[i])) {
+				count++
+			}
+		}
+		fmt.Println("Both DNK have", count, "common runes")
+	} else {
+		fmt.Println("error in the input strings")
+	}
+
+
 }
